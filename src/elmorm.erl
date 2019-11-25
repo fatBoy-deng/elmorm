ab78b1fcff1a1a2a38ed3fb2f317da69d42f86e7
fatBoy-deng <2508799160@qq.com>
2019-11-25 15:13:07 +0800

    export init

--- src/elmorm.erl
+++ src/elmorm.erl
@@ -1,12 +1,15 @@
 -module(elmorm).
 
 %% API exports
--export([]).
+-export([init/1]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
-
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State0) ->
+  {ok, State1} = elmorm_compile:init(State0),
+  {ok, State1}.
 
 %%====================================================================
 %% Internal functions
