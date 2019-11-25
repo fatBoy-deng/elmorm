d6d1736e532576dbd1f204825c20f9bb38aa6282
fatBoy-deng <2508799160@qq.com>
2019-11-25 14:46:02 +0800

    del elmorm_compile export

--- src/elmorm_compile.erl
+++ src/elmorm_compile.erl
@@ -2,42 +2,11 @@
 
 -include("elmorm.hrl").
 
--export([init/1, do/1, format_error/1]).
+-export([format_error/1]).
 -export([foo/0,parse_file/1,parse_binary/1]).
 
 -define(PROVIDER, elmorm).
 -define(DEPS, [app_discovery]).
-
-%% ===================================================================
-%% Public API
-%% ===================================================================
--spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
-    Provider = providers:create([
-        {name, ?PROVIDER},            % The 'user friendly' name of the task
-        {module, ?MODULE},            % The module implementation of the task
-        {bare, true},                 % The task can be run by the user, always true
-        {deps, ?DEPS},                % The list of dependencies
-        {example, "rebar3 elmorm"}, % How to use the plugin
-        {opts, []},                   % list of options understood by the plugin
-        {short_desc, "A rebar plugin"},
-        {desc, "A rebar plugin"}
-    ]),
-    {ok, rebar_state:add_provider(State, Provider)}.
-
-
--spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
-do(State) ->
-    Apps = case rebar_state:current_app(State) of
-               undefined ->
-                   rebar_state:project_apps(State);
-               AppInfo ->
-                   [AppInfo]
-           end,
-    lists:foreach(fun(App) ->
-        elmorm_compare:foo2(App, State)
-                  end, Apps),
-    {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
